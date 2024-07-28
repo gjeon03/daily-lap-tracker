a51b2be7d3c7c4502b8d2f68cf93e5516be66d3f
gyeongyeon <gjeon0321@gmail.com>
2024-07-29 02:27:53 +0900

    feat: number highlighting

--- src/components/number.tsx
+++ src/components/number.tsx
@@ -4,159 +4,271 @@ import { useEffect, useState } from "react";
 
 interface Props {
   size: number;
+  number: number;
 }
 
+/* Constants */
 const baseWidth = 8.5;
-const baseBorderWidth = 6.25;
+const baseBorderWidth = 7;
 const baseBorderHeight = 5;
 const baseBorder = 1;
+const baseMiddleHeight = 0.66;
 
-export default function Number({ size }: Props) {
+export default function Number({ size, number }: Props) {
+  /* Sizes */
   const width = baseWidth * size;
   const borderWidth = baseBorderWidth * size;
   const borderHeight = baseBorderHeight * size;
   const border = baseBorder * size;
+  const middleHeight = baseMiddleHeight * size;
+
+  /* Colors */
+  const [topColor, setTopColor] = useState("#F2F2F2");
+  const [topLeftColor, setTopLeftColor] = useState("#F2F2F2");
+  const [topRightColor, setTopRightColor] = useState("#F2F2F2");
+  const [middleColor, setMiddleColor] = useState("#F2F2F2");
+  const [bottomLeftColor, setBottomLeftColor] = useState("#F2F2F2");
+  const [bottomRightColor, setBottomRightColor] = useState("#F2F2F2");
+  const [bottomColor, setBottomColor] = useState("#F2F2F2");
+
+  useEffect(() => {
+    if (number === 0) {
+      setTopColor("#F2F2F2");
+      setTopLeftColor("#F2F2F2");
+      setTopRightColor("#F2F2F2");
+      setMiddleColor("#1E1E1E");
+      setBottomLeftColor("#F2F2F2");
+      setBottomRightColor("#F2F2F2");
+      setBottomColor("#F2F2F2");
+    } else if (number === 1) {
+      setTopColor("#1E1E1E");
+      setTopLeftColor("#1E1E1E");
+      setTopRightColor("#F2F2F2");
+      setMiddleColor("#1E1E1E");
+      setBottomLeftColor("#1E1E1E");
+      setBottomRightColor("#F2F2F2");
+      setBottomColor("#1E1E1E");
+    } else if (number === 2) {
+      setTopColor("#F2F2F2");
+      setTopLeftColor("#1E1E1E");
+      setTopRightColor("#F2F2F2");
+      setMiddleColor("#F2F2F2");
+      setBottomLeftColor("#F2F2F2");
+      setBottomRightColor("#1E1E1E");
+      setBottomColor("#F2F2F2");
+    } else if (number === 3) {
+      setTopColor("#F2F2F2");
+      setTopLeftColor("#1E1E1E");
+      setTopRightColor("#F2F2F2");
+      setMiddleColor("#F2F2F2");
+      setBottomLeftColor("#1E1E1E");
+      setBottomRightColor("#F2F2F2");
+      setBottomColor("#F2F2F2");
+    } else if (number === 4) {
+      setTopColor("#1E1E1E");
+      setTopLeftColor("#F2F2F2");
+      setTopRightColor("#F2F2F2");
+      setMiddleColor("#F2F2F2");
+      setBottomLeftColor("#1E1E1E");
+      setBottomRightColor("#F2F2F2");
+      setBottomColor("#1E1E1E");
+    } else if (number === 5) {
+      setTopColor("#F2F2F2");
+      setTopLeftColor("#F2F2F2");
+      setTopRightColor("#1E1E1E");
+      setMiddleColor("#F2F2F2");
+      setBottomLeftColor("#1E1E1E");
+      setBottomRightColor("#F2F2F2");
+      setBottomColor("#F2F2F2");
+    } else if (number === 6) {
+      setTopColor("#F2F2F2");
+      setTopLeftColor("#F2F2F2");
+      setTopRightColor("#1E1E1E");
+      setMiddleColor("#F2F2F2");
+      setBottomLeftColor("#F2F2F2");
+      setBottomRightColor("#F2F2F2");
+      setBottomColor("#F2F2F2");
+    } else if (number === 7) {
+      setTopColor("#F2F2F2");
+      setTopLeftColor("#1E1E1E");
+      setTopRightColor("#F2F2F2");
+      setMiddleColor("#1E1E1E");
+      setBottomLeftColor("#1E1E1E");
+      setBottomRightColor("#F2F2F2");
+      setBottomColor("#1E1E1E");
+    } else if (number === 8) {
+      setTopColor("#F2F2F2");
+      setTopLeftColor("#F2F2F2");
+      setTopRightColor("#F2F2F2");
+      setMiddleColor("#F2F2F2");
+      setBottomLeftColor("#F2F2F2");
+      setBottomRightColor("#F2F2F2");
+      setBottomColor("#F2F2F2");
+    } else if (number === 9) {
+      setTopColor("#F2F2F2");
+      setTopLeftColor("#F2F2F2");
+      setTopRightColor("#F2F2F2");
+      setMiddleColor("#F2F2F2");
+      setBottomLeftColor("#1E1E1E");
+      setBottomRightColor("#F2F2F2");
+      setBottomColor("#F2F2F2");
+    }
+  }, [number]);
 
   return (
     <div className="flex flex-col items-center" style={{ width: `${width}px` }}>
-      <div
-        className="border-b-secondary border-l-transparent border-r-transparent"
+      {/* <div
+        className="border-l-transparent border-r-transparent"
         style={{
           borderBottomWidth: `${border}px`,
           borderLeftWidth: `${border}px`,
           borderRightWidth: `${border}px`,
           width: `${borderWidth}px`,
+          borderBottomColor: topColor,
         }}
-      ></div>
+      ></div> */}
       <div
-        className="border-t-secondary border-l-transparent border-r-transparent"
+        className="border-l-transparent border-r-transparent"
         style={{
           borderTopWidth: `${border}px`,
           borderLeftWidth: `${border}px`,
           borderRightWidth: `${border}px`,
           width: `${borderWidth}px`,
+          borderTopColor: topColor,
         }}
-      ></div>
+      />
       <div className="flex justify-between w-full">
         <div className="flex">
-          <div
-            className="border-t-transparent border-r-secondary border-b-transparent"
+          {/* <div
+            className="border-t-transparent border-b-transparent"
             style={{
               borderTopWidth: `${border}px`,
               borderRightWidth: `${border}px`,
               borderBottomWidth: `${border}px`,
               height: `${borderHeight}px`,
+              borderRightColor: topLeftColor,
             }}
-          ></div>
+          ></div> */}
           <div
-            className="border-t-transparent border-l-secondary border-b-transparent"
+            className="border-t-transparent border-b-transparent"
             style={{
               borderTopWidth: `${border}px`,
               borderLeftWidth: `${border}px`,
               borderBottomWidth: `${border}px`,
               height: `${borderHeight}px`,
+              borderLeftColor: topLeftColor,
             }}
-          ></div>
+          />
         </div>
         <div className="flex">
           <div
-            className="border-t-transparent border-r-secondary border-b-transparent"
+            className="border-t-transparent border-b-transparent"
             style={{
               borderTopWidth: `${border}px`,
               borderRightWidth: `${border}px`,
               borderBottomWidth: `${border}px`,
               height: `${borderHeight}px`,
+              borderRightColor: topRightColor,
             }}
-          ></div>
-          <div
-            className="border-t-transparent border-l-secondary border-b-transparent"
+          />
+          {/* <div
+            className="border-t-transparent border-b-transparent"
             style={{
               borderTopWidth: `${border}px`,
               borderLeftWidth: `${border}px`,
               borderBottomWidth: `${border}px`,
               height: `${borderHeight}px`,
+              borderLeftColor: topRightColor,
             }}
-          ></div>
+          ></div> */}
         </div>
       </div>
       <div
-        className="border-b-secondary border-l-transparent border-r-transparent"
+        className="border-l-transparent border-r-transparent"
         style={{
-          borderBottomWidth: `${border}px`,
+          borderBottomWidth: `${middleHeight}px`,
           borderLeftWidth: `${border}px`,
           borderRightWidth: `${border}px`,
           width: `${borderWidth}px`,
+          borderBottomColor: middleColor,
         }}
-      ></div>
+      />
       <div
-        className="border-t-secondary border-l-transparent border-r-transparent"
+        className="border-l-transparent border-r-transparent"
         style={{
-          borderTopWidth: `${border}px`,
+          borderTopWidth: `${middleHeight}px`,
           borderLeftWidth: `${border}px`,
           borderRightWidth: `${border}px`,
           width: `${borderWidth}px`,
+          borderTopColor: middleColor,
         }}
-      ></div>
+      />
       <div className="flex justify-between w-full">
         <div className="flex">
-          <div
-            className="border-t-transparent border-r-secondary border-b-transparent"
+          {/* <div
+            className="border-t-transparent border-b-transparent"
             style={{
               borderTopWidth: `${border}px`,
               borderRightWidth: `${border}px`,
               borderBottomWidth: `${border}px`,
               height: `${borderHeight}px`,
+              borderRightColor: bottomLeftColor,
             }}
-          ></div>
+          ></div> */}
           <div
-            className="border-t-transparent border-l-secondary border-b-transparent"
+            className="border-t-transparent border-b-transparent"
             style={{
               borderTopWidth: `${border}px`,
               borderLeftWidth: `${border}px`,
               borderBottomWidth: `${border}px`,
               height: `${borderHeight}px`,
+              borderLeftColor: bottomLeftColor,
             }}
-          ></div>
+          />
         </div>
         <div className="flex">
           <div
-            className="border-t-transparent border-r-secondary border-b-transparent"
+            className="border-t-transparent border-b-transparent"
             style={{
               borderTopWidth: `${border}px`,
               borderRightWidth: `${border}px`,
               borderBottomWidth: `${border}px`,
               height: `${borderHeight}px`,
+              borderRightColor: bottomRightColor,
             }}
-          ></div>
-          <div
-            className="border-t-transparent border-l-secondary border-b-transparent"
+          />
+          {/* <div
+            className="border-t-transparent border-b-transparent"
             style={{
               borderTopWidth: `${border}px`,
               borderLeftWidth: `${border}px`,
               borderBottomWidth: `${border}px`,
               height: `${borderHeight}px`,
+              borderLeftColor: bottomRightColor,
             }}
-          ></div>
+          ></div> */}
         </div>
       </div>
       <div
-        className="border-b-secondary border-l-transparent border-r-transparent"
+        className="border-l-transparent border-r-transparent"
         style={{
           borderBottomWidth: `${border}px`,
           borderLeftWidth: `${border}px`,
           borderRightWidth: `${border}px`,
           width: `${borderWidth}px`,
+          borderBottomColor: bottomColor,
         }}
-      ></div>
-      <div
-        className="border-t-secondary border-l-transparent border-r-transparent"
+      />
+      {/* <div
+        className="border-l-transparent border-r-transparent"
         style={{
           borderTopWidth: `${border}px`,
           borderLeftWidth: `${border}px`,
           borderRightWidth: `${border}px`,
           width: `${borderWidth}px`,
+          borderTopColor: bottomColor,
         }}
-      ></div>
+      ></div> */}
     </div>
   );
 }
--- src/components/watch.tsx
+++ src/components/watch.tsx
@@ -4,13 +4,22 @@ export default function Watch() {
   return (
     <div>
       <h1>Watch</h1>
-      <div className="w-[400px] h-[400px] bg-yellow rounded-[100px] flex justify-center items-center">
-        <div className="w-[370px] h-[370px] bg-green rounded-[100px] flex justify-center py-8">
-          <div className="w-[300px] h-[200px] bg-primary rounded-[50px] flex justify-center items-center">
-            <Number size={6} />
-            {/* <Number size={8} />
-            <Number size={8} />
-            <Number size={8} /> */}
+      <div className="w-[420px] h-[400px] bg-yellow rounded-[100px] flex justify-center items-center">
+        <div className="w-[390px] h-[370px] bg-green rounded-[100px] flex justify-center py-8">
+          <div className="w-[340px] h-[200px] bg-primary rounded-[50px] flex justify-center items-center">
+            <div className="flex items-end justify-center gap-x-2 h-[80px]">
+              <Number size={6} number={1} />
+              <Number size={6} number={7} />
+              <div className="flex flex-col justify-center items-center gap-y-5 h-full">
+                <div className="bg-secondary w-[8px] h-[8px] rounded-full" />
+                <div className="bg-secondary w-[8px] h-[8px] rounded-full" />
+              </div>
+              <Number size={6} number={8} />
+              <Number size={6} number={9} />
+              <div className="bg-secondary w-[8px] h-[8px] rounded-full" />
+              <Number size={3} number={5} />
+              <Number size={3} number={6} />
+            </div>
           </div>
         </div>
       </div>
